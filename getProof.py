f8bf6916a819bd654bbd3b9ad9446e070dff86ea
Lejamon <Lejamon.c@protonmail.com>
2023-03-21 11:09:55 +0100

    add comments

--- getProof.py
+++ getProof.py
@@ -1,17 +1,12 @@
-import sys
+# This script imports all others. It then generates the proof and stores it on ipfs. The proof is then associated to an NFT, the URI being the ipfs link.
+
 from hackyaosring import haosring_sign
 import requests
 from get_data import getRing
 from getPoints import getPoints
 from mint_nft import mintNFT
-import ipfsApi
-#from ring_signature.hackyaosring import haosring_sign
 
-# pkey = [(115185473299647925444517437276795129670496237591884117069305290769490785372619,77315467441032930729971643386403848327495819100754888420609510070437620990962),(69842940699001444947118319642196642553073776903718332284726491807873962548236,1127970563606141788355784463430840738461580530412738073901802949073783286569)]
-# mykey =[(69842940699001444947118319642196642553073776903718332284726491807873962548236,1127970563606141788355784463430840738461580530412738073901802949073783286569),42071751069461095803574485567668923649610482576714143835877678793973757093411]
-# message = int("0x9198aEf8f3019f064d0826eB9e07Fb07a3d3a4BD",16)
-# proof = haosring_sign(pkeys=pkey,mypair=mykey,message=message)
-
+# function to send data to ipfs using infura gateway
 def sendToIpfs(data):
     files = {
         'file': data
@@ -19,6 +14,8 @@ def sendToIpfs(data):
     response = requests.post('https://ipfs.infura.io:5001/api/v0/add', files=files, auth=('2NEFcw6jsQRonyXpWguSouymY4o',"588a638882fff36f65b8c0c76aef28ce"))
     a = response.json()
     return a['Hash']
+    
+#function to generate the proof 
 def getProof(treshold, seedprivate, seedpublic):
     baseSet = getRing(treshold)
     points = getPoints(seedprivate)
